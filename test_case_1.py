e67053546f348be1cf8c4e025df0ac49113127e0
vv87 <38062151+vv87@users.noreply.github.com>
2020-06-27 23:33:14 +0300

    Fixed failure + create new assertion in test_case_1.py

--- test_case_1.py
+++ test_case_1.py
@@ -1,6 +1,9 @@
 import requests
 
 
-r = requests.get('https://api.github.com/event')
+r = requests.get('https://api.github.com')
+
 assert r.status_code == 200, ("No search results!")
+assert 'current_user_url' in r.json()
+
 print(r.json())
