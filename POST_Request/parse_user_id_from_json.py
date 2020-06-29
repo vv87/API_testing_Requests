5f5578f88c0ab03bf0a287f56fdb4608cbcb8c10
vv87 <38062151+vv87@users.noreply.github.com>
2020-06-29 19:36:45 +0300

    Update json file

---
+++ POST_Request/parse_user_id_from_json.py
@@ -0,0 +1,29 @@
+import requests
+import json
+import jsonpath
+
+# API URL
+url = "https://reqres.in/api/users"
+path_to_json_file = 'F:\\PythonProjects\\Requests_2\\POST_Request\\create_user_data.json'
+
+# Read input json file
+file = open(path_to_json_file, 'r')
+json_input = file.read()
+request_json = json.loads(json_input)
+
+# Make POST request with json input body
+response = requests.post(url, request_json)
+
+assert response.status_code == 201, 'Response code is not "201"!'
+
+# Parse Content-Length from response header
+print(response, '\nContent-Length:', response.headers.get('Content-Length'))
+
+# Parse response to json format
+response_json = json.loads(response.text)
+
+# Pick id using json path
+id = jsonpath.jsonpath(response_json, 'id')
+print(id[0])
+
+
---
+++ POST_Request/post_create_user.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+# API URL
+url = "https://reqres.in/api/users"
+path_to_json_file = 'F:\\PythonProjects\\Requests_2\\POST_Request\\create_user_data.json'
+
+# Read input json file
+file = open(path_to_json_file, 'r')
+json_input = file.read()
+request_json = json.loads(json_input)
+
+# Make POST request with json input body
+response_create_user = requests.post(url, request_json)
+print(response_create_user)
+
+# validating response code
+assert response_create_user.status_code == 201, 'Response code is not "201"!'
