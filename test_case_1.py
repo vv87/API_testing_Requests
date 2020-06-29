c9a9a13f8bcf7c8743ce22c3558f3dd8213d8320
vv87 <38062151+vv87@users.noreply.github.com>
2020-06-29 12:34:48 +0300

    Remove test_case_2.py & test_case_2.py from current branch as copy

--- test_case_1.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = 'https://api.github.com'
-r = requests.get(url)
-
-assert r.status_code == 200, ("Response code is not 200")
-assert 'current_user_url' in r.json()
-
-print(r.status_code, '\n', r.json())
