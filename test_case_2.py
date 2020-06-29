5d939969866dd2f937829e5bb7c3913cb96ee47d
vv87 <38062151+vv87@users.noreply.github.com>
2020-06-29 10:06:36 +0300

    Url rendered into a variable + update assert

--- test_case_2.py
+++ test_case_2.py
@@ -1,8 +1,9 @@
 import requests
 
 
-r = requests.get('https://api.vk.com/method/users.get?user_ids=210700286&fields=bdate&access_token=533bacf01e11f55b536a565b57531ac114461ae8736d6506a3&v=5.110')
-assert 'error' in r.json(), ("Not found")
+url = 'https://api.vk.com/method/users.get?user_ids=210700286&fields=bdate&access_token=533bacf01e11f55b536a565b57531ac114461ae8736d6506a3&v=5.110'
+r = requests.get(url)
+assert 'error' in r.json(), ("'error' Not found")
 assert r.status_code == 200
 
 print(r.status_code, "\n", r.json())
